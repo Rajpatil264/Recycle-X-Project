ccf75a28214ccdc502bd68de9a3dda4461cad19e
Rajvardhan <raj2003patil@gmail.com>
2025-02-09 14:25:49 +0530

    Done

--- CODE/backend_node/server.js
+++ CODE/backend_node/server.js
@@ -4,22 +4,22 @@ const reply = require("./models/responseStructure");
 const config = require("./App/appConfig");
 const jwt = require("jsonwebtoken");
 const app = express();
+const cors = require("cors");
 const commonRoutes = require("./routes/commanRoutes");
 const consumerRoutes = require("./routes/consumerRoutes");
 const supplierRoutes = require("./routes/supplierRoutes");
 
+// cors
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 // Middleware
 app.use(appendFileLogs);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// cors
-app.use((request, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Headers", "*");
-  response.setHeader("Access-Control-Allow-Methods", "*");
-  next();
-});
 
 app.use((request, response, next) => {
   if (request.url.includes("/signin") || request.url.includes("/signup") || request.url.includes("/verifyEmail")) {
--- CODE/frontend/recyclex_reactapp/src/App.js
+++ CODE/frontend/recyclex_reactapp/src/App.js
@@ -1,12 +1,16 @@
-
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Import routing components
 import UserTypeSelection from "./components/UserTypeSelection";
+import SupplierLogin from "./components/supplier/SupplierLogin"; // Create this component
 
 const App = () => {
   return (
-    <>
-      <UserTypeSelection></UserTypeSelection>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserTypeSelection />} />
+        <Route path="/supplier/login" element={<SupplierLogin />} />
+      </Routes>
+    </Router>
   );
 };
 
